22af74f5cb63a2b102e6527e24987f52e57e4b5c
Renato <reytuty@gmail.com>
2024-11-04 18:39:11 -0300

    chore: interfaces to import

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,9 @@
 import { IAppSdk } from "./interface/application/IAppSdk";
 import { IAccountSDK } from "./interface/sdk/IAccountSDK";
+import { ContextUserInfo, InteractionItem, IUserAccountDetails, IUserCredentials, UserInteractions } from "./interface/sdk/IUser";
+import { IContext, IProfile, IResult, IResultData } from "./interface/types";
 import { AccountSdk } from "./sdk/AccountSdk";
 import { AppSdk } from "./sdk/AppSdk";
 import { AccountMockSdk } from "./sdk/mocks/AccountMockSdk";
 import { AppMockSdk } from "./sdk/mocks/AppMockSdk";
-export { AccountSdk, AppSdk, IAppSdk, IAccountSDK, AccountMockSdk, AppMockSdk };
+export { AccountSdk, AppSdk, IAppSdk, IAccountSDK, AccountMockSdk, AppMockSdk, ContextUserInfo, IUserCredentials, IUserAccountDetails, IResult, IResultData, InteractionItem, UserInteractions, IContext, IProfile, };
